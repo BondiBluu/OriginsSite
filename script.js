efdfaf231073b6079b5cb9480c7553aa24a75bb2
Bami O. <bamideleogundeji949@gmail.com>
2023-10-12 12:15:38 -0400

    Made testing site

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPanel(){
+    document.getElementById("side-panel").style.left = "0px";
+}
+
+function closePanel(){
+    document.getElementById("side-panel").style.left = "-250px";
+}
